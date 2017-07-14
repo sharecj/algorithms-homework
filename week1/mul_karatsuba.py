f8cae2a443d9baada519408ccc54580492e7f556
yangshuo <wangchenkang@xuetangx.com>
2017-07-14 05:00:09 +0000

    finish week1 homework

---
+++ week1/mul_karatsuba.py
@@ -0,0 +1,15 @@
+#coding=utf8
+
+def mul(x, y):
+    if x<10 or y<10 or len(str(x)) % 2!=0 or len(str(y)) % 2!=0:
+        return x*y
+    length = len(str(x))
+    a = long(str(x)[:length/2])
+    b = long(str(x)[length/2:])
+    c = long(str(y)[:length/2])
+    d = long(str(y)[length/2:])
+    return pow(10, length) * mul(a,c) + pow(10, length/2) * (mul(a+b, c+d) - mul(a, c) - mul(b, d)) + mul(b, d)
+
+if __name__ == '__main__':
+    print mul(1789, 3243)
+    print mul(3141592653589793238462643383279502884197169399375105820974944592, 2718281828459045235360287471352662497757247093699959574966967627)
